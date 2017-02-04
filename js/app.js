8f124a9c12e0c1378bed1c5f650d1511e68c6256
maniyalraj <maniyalraj@gmail.com>
2017-02-04 22:51:40 +0530

    Box layout

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var app=angular.module('sevak',['ngRoute']);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "views/main.html"
+        templateUrl : "views/home.html"
     })
     .when("/main2", {
         templateUrl : "views/main2.html"
