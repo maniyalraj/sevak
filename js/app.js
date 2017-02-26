03780ee9ae05d9ae98e1158bb546b82c07dfce65
maniyalraj <maniyalraj@gmail.com>
2017-02-26 18:33:17 +0530

    Query DataBase on condition

--- js/app.js
+++ js/app.js
@@ -1,43 +1,91 @@
 var sql = require('sql.js');
-var fs= require('fs');
+var fs = require('fs');
 var filebuffer = fs.readFileSync('databasesElectiondatabase.db');
 
 var db = new sql.Database(filebuffer);
 
-var userProfile=process.env.USERPROFILE;
-if(!fs.existsSync(userProfile+'\\sevakData'))
-	{
-fs.mkdirSync(userProfile+'\\sevakData')
-	}
-var filePath=userProfile+'\\sevakData\\dataDump.json';
-var writeDataIntoFile = function (data)
-{
-	var JSONData=angular.toJson(data);
-	fs.writeFileSync(filePath,JSONData);
+var userProfile = process.env.USERPROFILE;
+if (!fs.existsSync(userProfile + '\\sevakData')) {
+	fs.mkdirSync(userProfile + '\\sevakData')
+}
+var filePath = userProfile + '\\sevakData\\dataDump.json';
+var writeDataIntoFile = function(data) {
+	var JSONData = angular.toJson(data);
+	fs.writeFileSync(filePath, JSONData);
+}
+
+var appendDataToFile = function(data) {
+	var JSONData = angular.toJson(data);
+	fs.appendFile(filePath, JSONData);
 }
 
 console.log(userProfile);
 
-var contents = db.exec("SELECT * FROM VoterList");
-
-var app=angular.module('sevak',['ngRoute']);
+var app = angular.module('sevak', [ 'ngRoute' ]);
 
 app.config(function($routeProvider) {
-    $routeProvider
-    .when("/", {
-        templateUrl : "views/home.html",
-        controller: "sevakController"
-    })
-    .when("/main2", {
-        templateUrl : "views/main2.html",
-        controller: "sevakController"
-    });
+	$routeProvider.when("/", {
+		templateUrl : "views/home.html",
+		controller : "sevakController"
+	}).when("/main2", {
+		templateUrl : "views/main2.html",
+		controller : "sevakController"
+	});
 });
 
-app.controller('sevakController',function($scope){
-	$scope.VoterListColumns=contents[0].columns;
-	$scope.VoterListValues=contents[0].values;
-	$scope.VoterListData=[];
+app.service('databaseService', function() {
+
+	var databaseService = {}
+
+	databaseService.getAllData = function(cb) {
+
+		var contents = db.exec("SELECT * FROM VoterList");
+		if (cb) {
+			cb(formatData(contents));
+		}
+	}
+
+	databaseService.queryOnCondition = function(queryCondition, cb) {
+
+		var contents = db.exec("Select * from VoterList WHERE "
+				+ queryCondition);
+
+		if (cb) {
+			cb(formatData(contents));
+		}
+	}
+
+	var formatData = function(data) {
+		var resultArray = []
+		for (var i = 0; i < data[0].values.length; i++) {
+			var temp = {};
+			for (var j = 0; j < data[0].columns.length; j++) {
+				var tempColumn = data[0].columns[j];
+				//tempColumn=trim(tempColumn);
+				temp[tempColumn] = data[0].values[i][j];
+			}
+			resultArray.push(temp);
+
+		}
+
+		return resultArray;
+	}
+
+	return databaseService;
+
+})
+app.controller('sevakController', function($scope, $http, databaseService) {
+
+	databaseService.getAllData(function(result) {
+		console.log(result);
+		$scope.VoterListData = result.data;
+	});
+
+	databaseService.queryOnCondition('SectionNo=17', function(result) {
+		console.log(result);
+
+	});
+
 	$scope.search = {
 		ConstNo : '',
 		PartNo : '',
@@ -62,19 +110,16 @@ app.controller('sevakController',function($scope){
 		IMPVoter : ''
 
 	};
-	for(var i=0; i<$scope.VoterListValues.length; i++)
-		{
-			var temp={};
-			for(var j=0;j<$scope.VoterListColumns.length; j++)
-				{	
-					var tempColumn= $scope.VoterListColumns[j];
-					//tempColumn=trim(tempColumn);
-					temp[tempColumn]=$scope.VoterListValues[i][j];
-				}
-			$scope.VoterListData.push(temp);
-		
-		}
+
 	writeDataIntoFile($scope.VoterListData);
-	
-	
+
+	console.log("Fetching from file");
+
+	$http.get(filePath).then(function(result) {
+
+		console.log("Fetching Completed");
+		$scope.VoterListData = result.data;
+
+	});
+
 })
